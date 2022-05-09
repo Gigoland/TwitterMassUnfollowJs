9edbeaf54e490a48491f7b4d2485a623aeec86be
GigolNft <gigolnet@gmail.com>
2022-05-09 22:13:53 +0200

    Comments

--- console.js
+++ console.js
@@ -1,3 +1,6 @@
+// 1. Go to https://twitter.com/%your%/following list age
+// 2. Paste this code in browser console
+// 3. Enter
 document.querySelectorAll("[data-testid*='unfollow']").forEach(function(ev) {
 	ev.click();
   $("div[data-testid='confirmationSheetConfirm']").click();
