4077ba43b946c25d2481aff04fed9cbb2185d60b
GigolNft <gigolnet@gmail.com>
2022-05-09 22:14:43 +0200

    refacto

--- console.js
+++ console.js
@@ -2,6 +2,6 @@
 // 2. Paste this code in browser console
 // 3. Enter
 document.querySelectorAll("[data-testid*='unfollow']").forEach(function(ev) {
-	ev.click();
+  ev.click();
   $("div[data-testid='confirmationSheetConfirm']").click();
 });
