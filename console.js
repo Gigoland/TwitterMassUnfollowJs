6fb591a01edf8e687144c99980e874892f77764b
GigolNft <gigolnet@gmail.com>
2022-05-09 22:17:08 +0200

    final

--- console.js
+++ console.js
@@ -1,4 +1,4 @@
-// 1. Go to https://twitter.com/%your%/following list age
+// 1. Go to https://twitter.com/%your%/following list page
 // 2. Paste this code in browser console
 // 3. Enter
 document.querySelectorAll("[data-testid*='unfollow']").forEach(function(ev) {
